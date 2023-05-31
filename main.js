223b91996ef578bf107acb0b413e9c08fd41e01b
Kyle-07 <125091796+Kyle-07@users.noreply.github.com>
2023-05-30 22:45:38 -0600

    Update main.js

--- main.js
+++ main.js
@@ -3,30 +3,56 @@ const canvas = document.getElementById("canvas");
 const ctx = canvas.getContext("2d");
 canvas.width = innerWidth;
 canvas.height = innerHeight;
+let Score = 0
 
 function animate() {
-  ctx.fillStyle = "#FF0000";
-  ctx.fillRect(0, 0, canvas.width, canvas.height);
-  let randX = Math.random() * canvas.width;
-  let randY = Math.random() * canvas.height;
 
+  let randX = Math.random() * canvas.width + 100;
+  let randY = Math.random() * canvas.height + 100;
   function SpawnTargets() {
+  randX = Math.random() * canvas.width  ;
+  randY = Math.random() * canvas.height ;
+
+  if (randX < 100){ randX += 100
+  }
+else { randX -= 100}
+
+if (randY < 100){ randY += 100
+}
+else { randY -= 100}
+
+
+
+
+
+
+    ctx.fillStyle = "#FF0000";
+    ctx.fillRect(0, 0, canvas.width, canvas.height);
+
     ctx.fillStyle = "blue";
 
     ctx.fillRect(randX, randY, 100, 100);
-    console.log("drawn");
+
+    ctx.font = "30px Tahoma";
+    ctx.fillStyle = "white";
+    ctx.fillText(`Score: ${Score}`, canvas.width / 2 - 65, 40);
   }
 
   addEventListener("click", Clicked);
   function Clicked(event) {
     let x = event.clientX;
     let y = event.clientY;
-    if (x > randX && x < randX + 100) {
+    if ((x > randX && x < randX + 100) && (y > randY && y < randY + 100)) {
+ctx.clearRect(0, 0, canvas.width, canvas.height)
       SpawnTargets();
+      Score += 10
+      console.log(Score)
     }
   }
 
   SpawnTargets();
+
 }
 
 animate();
+
