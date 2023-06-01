0cce51a17193439857e933e1dae209ea5a2fd51d
Kyle-07 <125091796+Kyle-07@users.noreply.github.com>
2023-06-01 14:41:16 -0600

    Update main.js

--- main.js
+++ main.js
@@ -1,21 +1,34 @@
 // Aim Trainer
 const canvas = document.getElementById("canvas");
 const ctx = canvas.getContext("2d");
-canvas.width = innerWidth;
-canvas.height = innerHeight;
+canvas.width = innerWidth - 15;
+canvas.height = innerHeight - 4;
 let Score = 0;
 let time = 0;
-let timesClicked = 0
-let ClickedBox = 0 
+let timesClicked = 0;
+let ClickedBox = 0;
+let accuracy = 0;
 
+ctx.fillStyle = "blue";
+ctx.fillRect(0, 0, canvas.width, canvas.height);
+ctx.font = "30px Tahoma";
+ctx.fillStyle = "white";
+ctx.fillText(`Press Space to Start`, canvas.width / 2 - 65, 70);
 
+document.addEventListener("keydown", keydownHandler1);
+function keydownHandler1(event) {
+  console.log(event);
+  if (event.code == "Space") {
+    animate();
+  }
+}
 
 function animate() {
+  accuracy = 0;
   time = 0;
   Score = 0;
-  timesClicked = 0
-  ClickedBox = 0 
-
+  timesClicked = 0;
+  ClickedBox = 0;
 
   let randX = Math.random() * canvas.width + 100;
   let randY = Math.random() * canvas.height + 100;
@@ -35,44 +48,24 @@ function animate() {
     } else {
       randY -= 100;
     }
-//Background
+    //Background
     ctx.fillStyle = "#FF0000";
     ctx.fillRect(0, 0, canvas.width, canvas.height);
-//Square
+    //Square
     ctx.fillStyle = "blue";
-    ctx.fillRect(randX, randY, 100, 100);
-//Text
+    ctx.fillRect(randX, randY, 35, 35);
+    //Text
     ctx.font = "30px Tahoma";
     ctx.fillStyle = "white";
     ctx.fillText(`Score: ${Score}`, canvas.width / 2 - 65, 40);
     ctx.fillStyle = "white";
     ctx.fillText(`Time: ${20 - time}`, canvas.width / 2 - 65, 70);
-
   }
 
-document.addEventListener("click", Clicked);
-  function Clicked(event) {
-    let x = event.clientX;
-    let y = event.clientY;
-    if (x > randX && x < randX + 100 && y > randY && y < randY + 100) {
-      ctx.clearRect(0, 0, canvas.width, canvas.height);
-      Score += 10;
-      SpawnTargets();
-      timesClicked += 1 ;
-      ClickedBox += 1  ;
-    } else { 
-      timesClicked += 1
-    
-    }
-    console.log(timesClicked) 
-    console.log(ClickedBox) 
-  }
+  myInterval = setInterval(Timer, 1000);
 
-let accuracy = (ClickedBox / timesClicked * 100)
-
-
-let clear = setInterval(Timer, 1000);
   function Timer() {
+    accuracy = Math.round((ClickedBox / timesClicked) * 100);
     if (time < 20) {
       time += 1;
       ctx.fillText(`Time: ${20 - time}`, canvas.width / 2 - 65, 70);
@@ -81,29 +74,51 @@ let clear = setInterval(Timer, 1000);
       ctx.fillStyle = "white";
       ctx.fillText(`Time: ${20 - time}`, canvas.width / 2 - 65, 70);
     } else {
-      console.log(accuracy) 
       ctx.clearRect(0, 0, canvas.width, canvas.height);
       ctx.fillStyle = "blue";
       ctx.fillRect(0, 0, canvas.width, canvas.height);
       ctx.fillStyle = "white";
       ctx.fillText(`Press Space to Restart`, canvas.width / 2 - 65, 70);
-      ctx.fillText(`${accuracy}% Accuracy`, canvas.width / 2 - 65, 100);
+      ctx.fillText(`Score: ${Score}`, canvas.width / 2 - 65, 130);
+      if (accuracy > 0) {
+        ctx.fillText(`${accuracy}% Accuracy`, canvas.width / 2 - 65, 100);
+        accuracy = 0;
+      } else {
+        ctx.fillText(`0% Accuracy`, canvas.width / 2 - 65, 100);
+        accuracy = 0;
+      }
       randX = -10000000;
       randY = -1000000;
-   
+      clearInterval(myInterval);
     }
-   
   }
-  document.addEventListener("keydown", keydownHandler)
 
-function keydownHandler(event){
-  if (time >= 20) {
-  if (event.code == "Space"){
-    clearInterval(clear)
-animate()
+  document.addEventListener("click", Clicked);
+  function Clicked(event) {
+    let x = event.clientX;
+    let y = event.clientY;
+    if (x > randX && x < randX + 100 && y > randY && y < randY + 100) {
+      ctx.clearRect(0, 0, canvas.width, canvas.height);
+      Score += 1;
+      SpawnTargets();
+      timesClicked += 1;
+      ClickedBox += 1;
+    } else {
+      timesClicked += 1;
+    }
+    console.log(timesClicked);
+    console.log(ClickedBox);
   }
-}
-}
+
+  document.addEventListener("keydown", keydownHandler);
+
+  function keydownHandler(event) {
+    if (time >= 20) {
+      if (event.code == "Space") {
+        clearInterval(myInterval);
+        animate();
+      }
+    }
+  }
   SpawnTargets();
 }
-animate();
